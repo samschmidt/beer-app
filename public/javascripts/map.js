d286f171bbbb0371ba2f1b169d79624eafc27407
Sam <schmice@mail.uc.edu>
2016-07-19 17:11:33 -0400

    Commenting and cleaning, mostly in map.js

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,13 +1,15 @@
-//map.js
-/*
- * This file contains code that relates to map functionality.
- *
- *
- */
+/* map.js
+  * 
+  * This file contains code that relates to map functionality.
+  * This includes the map definition and the code the plots
+  *  breweries onto the map. No beers or recommendations should be
+  *  manipulated here.
+  */
 
-//Global vars
-//Holds the bounds to be used once all breweries are mapped
+//Global map vars
+//Holds the map's bounds used once all breweries are mapped
 var bounds;// = new google.maps.LatLngBounds();
+//Array that holds all markers displayed on map
 var markers = [];
 
 var map;
@@ -43,136 +45,12 @@ function initMap() {
 
 }//initMap();
 
-//Search the BreweryDB for breweries within default range of latLng
-function breweryDbSearchByLatLng(latLng)
-{
-  //Jquery AJAX
-  $.get('/searchLatLng?lat=' + latLng.lat() + "&lng=" + latLng.lng(),
-        plotBreweries,
-        "json");
-}
 
-function clearMarkersFromMap()
-{
-  markers.forEach( function(element, index, array) {
-    element.setMap(null);
-  })
-}
-
-
-function plotBrewery(element, index, array)
-{
-  console.log("\t"+element.brewery.name);
-  console.log("\t"+element.latitude + "\t" + element.longitude);
-
-  breweryLatLng = new google.maps.LatLng({lat: element.latitude, lng: element.longitude});
-
-
-  var marker = new google.maps.Marker({
-    position: breweryLatLng,
-    map: map,
-    title: element.brewery.name
-  });
-
-  markers.push(marker);
-
-  bounds.extend(breweryLatLng);
-
-
-  // var infowindow = new google.maps.InfoWindow({
-  //   content: 'This is the location of RG'
-  // })
-
-  // marker.addListener('click', function() {
-  //   infowindow.open(map, marker);
-  // })
-}
-
-//Plot the breweries from BreweryDB onto the map
-function plotBreweries(response)
-{
-  console.log(response);
-
-  console.log("Plot the breweries!");
-
-  //If no breweries found
-  //TODO display some better message
-  if (response.data === undefined)
-  {
-    alert('Sorry, we could find no breweries in that area!');
-    return;
-  }
-  //Save breweries from response
-  breweries = response.data;
-
-  //clear the markers and the old bounds
-  clearMarkersFromMap();
-  bounds = new google.maps.LatLngBounds();
-
-  breweries.forEach(plotBrewery);
-
-  //fit bounds to the mapped breweries
-  map.fitBounds(bounds);
-
-
-  //Clear breweries from list
-  $('#results-list').empty();
-
-  //Display the first paginationSize breweries in search results
-  for (var i=0; i<paginationSize; i++)
-  {
-    listBrewery(breweries[i]);
-  }
-
-
-  //Clear pagination numbers
-  $('#results-pagination li.pagination-item').remove();
-
-  //add a nav button for every paginationSize breweries, up to 5
-  for (var i=0; i<breweries.length/paginationSize && i<5; i++)
-  {
-    var paginationToAdd = $.parseHTML(paginationTemplate);
-
-    $(paginationToAdd).children().html(i+1);
-
-    $('#results-pagination .right-arrow').before(paginationToAdd);
-  }
-
-  //set the current page variable
-  currentPage = 1;
-  $('#results-pagination .pagination-item').first().addClass('active');
-
-  //Select the breweries tab
-  showTabBreweries();
-
-
-  // console.log("\t"+breweriesArr[0].brewery.name);
-  // console.log("\t"+breweriesArr[0].latitude + "\t" + breweriesArr[0].longitude);
-
-
-  // var marker = new google.maps.Marker({
-  //   position: {lat: breweriesArr[0].latitude, lng: breweriesArr[0].longitude},
-  //   map: map,
-  //   title: breweriesArr[0].name
-  // });
-
-  // var infowindow = new google.maps.InfoWindow({
-  //   content: 'This is the location of RG'
-  // })
-
-  // marker.addListener('click', function() {
-  //   infowindow.open(map, marker);
-  // })
-
-
-}
-
-
-
-
-
-
-//Find nearby breweries and zoom to their area
+/** 
+ * This function gets us started. It zooms to the user's entered location and plots 
+ *  the breweries in that location.
+ * Called when the user clicks the search button or hits enter.
+ */
 function zoomToArea() {
   console.log("Zooming to the user's location");
 
@@ -210,8 +88,11 @@ function zoomToArea() {
 }
 
 
-//When the user presses enter in the location box, trigger the search
 $( document ).ready( function() {
+  
+  /**
+    * When the user presses enter in the location box, trigger the search.
+    */
   $("#zoom-to-location-txt").bind("keypress", {}, keypressInBox);
 
   function keypressInBox(e) {
@@ -227,17 +108,116 @@ $( document ).ready( function() {
 });
 
 
+/**
+ * Remove all markers from the map object
+ */
+function clearMarkersFromMap()
+{
+  markers.forEach( function(element, index, array) {
+    element.setMap(null);
+  })
+}
+
+
+/**
+ * Search the BreweryDB for breweries within default range of latLng.
+ * Callback gives control to plotBreweries()
+ */
+function breweryDbSearchByLatLng(latLng)
+{
+  //Jquery AJAX
+  $.get('/searchLatLng?lat=' + latLng.lat() + "&lng=" + latLng.lng(),
+        plotBreweries,
+        "json");
+}
+
+/**
+ * Display all found breweries on the map.
+ * This function is the callback for the latlng search.
+ */
+function plotBrewery(element, index, array)
+{
+  console.log("\t"+element.brewery.name);
+  console.log("\t"+element.latitude + "\t" + element.longitude);
+
+  //Object to be mapped
+  breweryLatLng = new google.maps.LatLng({lat: element.latitude, lng: element.longitude});
+
+  var marker = new google.maps.Marker({
+    position: breweryLatLng,
+    map: map,
+    title: element.brewery.name
+  });
+
+  // var infowindow = new google.maps.InfoWindow({
+  //   content: 'This is the location of RG'
+  // })
+
+  // marker.addListener('click', function() {
+  //   infowindow.open(map, marker);
+  // })
+
+
+  markers.push(marker);
+
+  bounds.extend(breweryLatLng);
+}
+
+/**
+ * Plot the breweries from BreweryDB onto the map.
+ * Also fill the in the appropriate pagination information, including
+ *  setting globals and drawing the pagination elements.
+ */
+function plotBreweries(response)
+{
+  console.log(response);
+
+  console.log("Plot the breweries!");
+
+  //If no breweries found
+  //TODO display some better message
+  if (response.data === undefined)
+  {
+    alert('Sorry, we could find no breweries in that area!');
+    return;
+  }
+
+  //Clear the markers and the old bounds
+  clearMarkersFromMap();
+  bounds = new google.maps.LatLngBounds();
+
+  //Save breweries from response to the global array
+  breweries = response.data;
+  //Set pagination array to be breweries
+  currentArray = breweries;
+
+
+
+  //Plot breweries and fit the map bounds around them
+  breweries.forEach(plotBrewery);
+  map.fitBounds(bounds);
+
+
+  //Set the current pagination variable
+  // currentPage = 1;
+  // $('#results-pagination .pagination-item').first().addClass('active');
+
+  //Select the breweries tab
+  showTabBreweries();
+  listResults();
+}
+
 ////////////////////////////
 ///OLD OR DEPRECATED CODE///
 ////////////////////////////
 
 //prelim testing - find breweries by postal code
-function breweryDbLocationSearchByPostalCode(loc)
-{
-  $.get('/dbreq?postalCode=' + loc,
-    function(data) {
-      alert( "Load was performed." + data );
-      console.log(data);
-    },
-    "json");
-}
+// function breweryDbLocationSearchByPostalCode(loc)
+// {
+//   $.get('/dbreq?postalCode=' + loc,
+//     function(data) {
+//       alert( "Load was performed." + data );
+//       console.log(data);
+//     },
+//     "json");
+// }
