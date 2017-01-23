77de9eb1eaf4d5259d772d516d7cebdc6b12a9c7
Samuel Schmidt <tgcapnsam@gmail.com>
2017-01-23 18:59:06 -0500

    Continued work on map infowindow, refactor css into new stylesheet

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -251,13 +251,44 @@ function plotBrewery(element, index, array)
        // Moves the infowindow 115px to the right.
        iwOuter.parent().parent().css({left: '115px'});
        // Moves the shadow of the arrow 76px to the left margin
-      iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
-      // Moves the arrow 76px to the left margin
-      iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
-      // Changes the desired color for the tail outline.
-      // The outline of the tail is composed of two descendants of div which contains the tail.
-      // The .find('div').children() method refers to all the div which are direct descendants of the previous div.
-      iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+       iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+       // Moves the arrow 76px to the left margin
+       iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+       // Changes the desired color for the tail outline.
+       // The outline of the tail is composed of two descendants of div which contains the tail.
+       // The .find('div').children() method refers to all the div which are direct descendants of the previous div.
+       iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
+
+
+
+      // Taking advantage of the already established reference to
+      // div .gm-style-iw with iwOuter variable.
+      // You must set a new variable iwCloseBtn.
+      // Using the .next() method of JQuery you reference the following div to .gm-style-iw.
+      // Is this div that groups the close button elements.
+      var iwCloseBtn = iwOuter.next();
+
+      // Apply the desired effect to the close button
+      iwCloseBtn.css({
+        width: '19px',
+        height: '19px',
+        overflow: 'hidden',
+        position: 'absolute',
+        opacity: '1', // by default the close button has an opacity of 0.7
+        right: '25px', top: '3px', // button repositioning
+        border: '3px solid #00274c', // increasing button border and new color
+        'border-radius': '13px', // circular effect
+        'box-shadow': '0 0 5px #3990B9' // 3D effect to highlight the button
+        });
+
+      // The API automatically applies 0.7 opacity to the button after the mouseout event.
+      // This function reverses this event to the desired value.
+      iwCloseBtn.mouseout(function(){
+        $(this).css({opacity: '1'});
+      });
+
+
+
     });
 
 
