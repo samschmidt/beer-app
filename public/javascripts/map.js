b60b0b5fe8f9fc32c12418c4743415b4e019e6bd
Samuel Schmidt <tgcapnsam@gmail.com>
2017-01-23 20:04:15 -0500

    Finish basasic frame of map infowindows

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -234,39 +234,39 @@ function plotBrewery(element, index, array)
       content: infoWindowContentStr
     });
 
-    // Add a listener to clean up the infowindow slightly when it's loaded
+    // Add a listener to clean up and restyle the infowindow when it's loaded
     // http://en.marnoto.com/2014/09/5-formas-de-personalizar-infowindow.html
     google.maps.event.addListener(infoWindowGlobal, 'domready', function() {
 
        // Reference to the DIV which receives the contents of the infowindow using jQuery
        var iwOuter = $('.gm-style-iw');
        var iwBackground = iwOuter.prev();
-
        // Remove the background shadow DIV
        iwBackground.children(':nth-child(2)').css({'display' : 'none'});
-
        // Remove the white background DIV
        iwBackground.children(':nth-child(4)').css({'display' : 'none'});
 
        // Moves the infowindow 115px to the right.
        iwOuter.parent().parent().css({left: '115px'});
        // Moves the shadow of the arrow 76px to the left margin
-       iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+       iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 50px !important;'});
        // Moves the arrow 76px to the left margin
-       iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+       iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 50px !important;'});
        // Changes the desired color for the tail outline.
        // The outline of the tail is composed of two descendants of div which contains the tail.
        // The .find('div').children() method refers to all the div which are direct descendants of the previous div.
        iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1'});
 
 
-
-      // Taking advantage of the already established reference to
-      // div .gm-style-iw with iwOuter variable.
-      // You must set a new variable iwCloseBtn.
       // Using the .next() method of JQuery you reference the following div to .gm-style-iw.
       // Is this div that groups the close button elements.
       var iwCloseBtn = iwOuter.next();
+
+      // Style the outer div
+      iwOuter.css({
+        right: '115px',
+        left: 'initial'
+      });
 
       // Apply the desired effect to the close button
       iwCloseBtn.css({
@@ -275,7 +275,7 @@ function plotBrewery(element, index, array)
         overflow: 'hidden',
         position: 'absolute',
         opacity: '1', // by default the close button has an opacity of 0.7
-        right: '25px', top: '3px', // button repositioning
+        right: '105px', top: '3px', // button repositioning
         border: '3px solid #00274c', // increasing button border and new color
         'border-radius': '13px', // circular effect
         'box-shadow': '0 0 5px #3990B9' // 3D effect to highlight the button
@@ -286,11 +286,7 @@ function plotBrewery(element, index, array)
       iwCloseBtn.mouseout(function(){
         $(this).css({opacity: '1'});
       });
-
-
-
     });
-
 
     infoWindowGlobal.open(map, marker);
   });
